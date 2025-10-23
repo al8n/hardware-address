4a6d656898747112f6e2755d913aa8f5c1800a5f
al8n <scygliu1@gmail.com>
2025-10-23 15:34:15 +1300

    fix py ci

--- python/src/lib.rs
+++ python/src/lib.rs
@@ -2,6 +2,7 @@ use ::hardware_address::*;
 use pyo3::prelude::*;
 
 #[pymodule]
+#[pyo3(name = "hardware_address")]
 fn hardware_address(m: &Bound<'_, PyModule>) -> PyResult<()> {
   m.add_class::<MacAddr>()?;
   m.add_class::<Eui64Addr>()?;
